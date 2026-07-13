8e1decf7691057b681b5a96dbc8c10570ae40bbd
Aly <me@s5.pm>
2026-07-13 03:59:45 -0600

    flake update, sbt to 2.0.2

--- pkgs/sbt.nix
+++ pkgs/sbt.nix
@@ -7,11 +7,11 @@
   jdk-26 = javaPackages.compiler.temurin-bin.jdk-26;
 in
   (pkgs.sbt.overrideAttrs (finalAttrs: prevAttrs: {
-    version = "2.0.1";
+    version = "2.0.2";
 
     src = fetchurl {
       url = "https://github.com/sbt/sbt/releases/download/v${finalAttrs.version}/sbt-${finalAttrs.version}.tgz";
-      hash = "sha256-dQ7GGY12eaTBgQuNTWNJGK0SGw18ZN9onAur3rhF17g=";
+      hash = "sha256-2Ac//RIs6pUONEQPs/4UEgxHGgvyhDxy0n8lg17nJPk=";
     };
 
     passthru =
